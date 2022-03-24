43f02d860da95b63e107a1dbc159636c6dbe113e
lakshya123 <duggallakshya@gmail.com>
2022-03-24 12:57:49 +0530

    clock running

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    const state = useState();
 
     const [time, setTime] = useState(new Date().toLocaleTimeString());
-
-    // const [time, setTime] = useState(time);
 
     const UpdateTime = () => {
         setTime(new Date().toLocaleTimeString())
     } 
 
-
+    setInterval(UpdateTime,1000);
     return (
         <>
             <div className="card">
                 <h1> {time} </h1>
-                <button onClick={UpdateTime}> Get Time </button>
             </div>
         </>
     )
